ab1a68fc2786a3251397a372ad72aee6aa6ef7e2
Natalia <n.platova@hotmail.com>
2019-10-15 19:45:34 +0700

    cards onclick

---
+++ index.js
@@ -0,0 +1,14 @@
+$(window).on("load", ()=>{
+    $(".game-card").on("click", function() {
+        $(".overlay").css("display", "block");
+        let clickedGameCard = $(this);
+        let imageSrc = clickedGameCard[0].children[0].attributes[0].value;
+        let gameTitle = clickedGameCard[0].children[1].children[0].innerText;
+        let gamePrice = clickedGameCard[0].children[1].children[1].innerText;
+
+        let overlayedCardImage = $('#inf-card-image');
+        $('#gameName').html(gameTitle);
+        $('#gamePrice').html(gamePrice);
+        overlayedCardImage[0].attributes[0].value = imageSrc;
+    });
+});
